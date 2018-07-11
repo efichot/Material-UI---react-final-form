4ce0ef3a28c80e7da006ae5c644feb262f35c01e
efichot <fichotetienne@gmail.com>
2018-07-11 17:52:05 +0200

    modif for eslint

--- src/components/Exercices/LeftPane.js
+++ src/components/Exercices/LeftPane.js
@@ -17,14 +17,13 @@ export default ({ styles }) => (
       render={({ handleSubmit, form, submitting, pristine, values }) => (
         <form onSubmit={handleSubmit}>
           <div>
-            <label>First Name</label>
-            <Field name="firstName" component="input" type="text" placeholder="First Name" />
+            <label htmlFor="name">First Name</label>
+            <Field name="firstName" component="input" type="text" placeholder="First Name" id="name" />
           </div>
           <div>
             <label>Last Name</label>
             <Field name="lastName" component="input" type="text" placeholder="Last Name" />
           </div>
-
           <div className="buttons">
             <button type="submit" disabled={submitting || pristine}>
               Submit
